61088bff276559c8502d12f1b9fd6d314b7fc2c6
Alex <Alex110397@gmail.com>
2019-04-06 22:15:25 +0200

    update : add webpack

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
+
+
+
+
+
+require('electron-reload')(__dirname);
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -16,7 +22,7 @@ function createWindow () {
     }
   })
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
